cecc6c79d5d414ad9213e4c38cad463b87adfce7
mariopenguin <mariomatamosquitos@hotmail.es>
2021-10-25 10:03:08 +0200

    add test

--- src/test/java/com/practica/cajanegra/addTest.java
+++ src/test/java/com/practica/cajanegra/addTest.java
@@ -2,19 +2,35 @@ package com.practica.cajanegra;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
-import com.cajanegra.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class addTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private SingleLinkedListImpl<String> listaTest; 
+	
+	@BeforeEach
+	public void setUp() {
+		this.listaTest = new SingleLinkedListImpl<String>("A","B","C");
 	}
+	
 	@DisplayName("Single test successful")
 	@Test
 	void testSingleSuccessTest() {
 		assertEquals("patata","patata");
 	}
+	
+	@ParameterizedTest(name="addLast {0} in list")
+	@ValueSource(strings= {"@", "A", "B", "M", "Y", "Z", "[","1938"})
+	public void addLast(String s) {
+		this.listaTest.addLast(s);
+		assertEquals("[A, B, C, " + s + "]", this.listaTest.toString());
+	}
+
+	
 }
