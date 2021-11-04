6f29533af773aee7a3698c9aa04b1d1b49027bb8
echoliz <79090146+echoliz@users.noreply.github.com>
2021-11-04 15:38:38 +0100

    Substring tests added

--- src/test/java/com/practica/cajanegra/OtherTests.java
+++ src/test/java/com/practica/cajanegra/OtherTests.java
@@ -1,72 +1,73 @@
 package com.practica.cajanegra;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import com.cajanegra.SingleLinkedListImpl;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Tests que incluyen los del tipo isEmpty, size y toString")
 class OtherTests {
 
-	private SingleLinkedListImpl<String> listaTest,listaVacia,listaUnElemento; 
-	
-	@BeforeEach
-	public void setUp() { 
-		this.listaTest = new SingleLinkedListImpl<String>("A","B","C");
-		this.listaVacia = new SingleLinkedListImpl<String>();
-		this.listaUnElemento = new SingleLinkedListImpl<String>("A");
-	}
+    private SingleLinkedListImpl<String> listaTest, listaVacia, listaUnElemento;
 
-	@Nested
-	@DisplayName("isEmpty()")
-	class isEmpty{
-		@Test
-		public void isEmptyCasoVacio(){
-			assertTrue(listaVacia.isEmpty());
-		}
-		@Test
-		public void isEmptyCasoLlenoyDeUnSoloElemento(){ //En los dos casos el código es igual
-			assertFalse(listaUnElemento.isEmpty());
-		}
-	}
+    @BeforeEach
+    public void setUp() {
+        this.listaTest = new SingleLinkedListImpl<String>("A", "B", "C");
+        this.listaVacia = new SingleLinkedListImpl<String>();
+        this.listaUnElemento = new SingleLinkedListImpl<String>("A");
+    }
 
-	@Nested
-	@DisplayName("getSize()")
-	class size{
-		@Test
-		public void getSizeCasoVacio(){
-			assertEquals(0, listaVacia.size());
-		}
-		@Test
-		public void getSizeCasoUnElemento(){
-			assertEquals(1, listaUnElemento.size());
-		}
-		@Test
-		public void getSizeCasoLleno() {
-			assertEquals(3, listaTest.size());//Comprobar el get size con 50?
-		}
-	}
+    @Nested
+    @DisplayName("isEmpty()")
+    class isEmpty {
+        @Test
+        public void isEmptyCasoVacio() {
+            assertTrue(listaVacia.isEmpty());
+        }
+
+        @Test
+        public void isEmptyCasoLlenoyDeUnSoloElemento() { //En los dos casos el código es igual
+            assertFalse(listaUnElemento.isEmpty());
+        }
+    }
+
+    @Nested
+    @DisplayName("getSize()")
+    class size {
+        @Test
+        public void getSizeCasoVacio() {
+            assertEquals(0, listaVacia.size());
+        }
+
+        @Test
+        public void getSizeCasoUnElemento() {
+            assertEquals(1, listaUnElemento.size());
+        }
+
+        @Test
+        public void getSizeCasoLleno() {
+            assertEquals(3, listaTest.size());//Comprobar el get size con 50?
+        }
+    }
 
 
-	@Nested
-	@DisplayName("toString() de [A, B, C]")
-	class toString{
-		@Test
-		public void toStringCasoVacio(){
-			assertEquals("[]",listaVacia.toString());
-		}
+    @Nested
+    @DisplayName("toString() de [A, B, C]")
+    class toString {
+        @Test
+        public void toStringCasoVacio() {
+            assertEquals("[]", listaVacia.toString());
+        }
 
-		@Test
-		public void toStringCasoLleno() {
-			assertEquals("[A, B, C]",listaTest.toString());
-		}
-	}
+        @Test
+        public void toStringCasoLleno() {
+            assertEquals("[A, B, C]", listaTest.toString());
+        }
+    }
 
 }
--- src/test/java/com/practica/cajanegra/ReverseAndRemoveTests.java
+++ src/test/java/com/practica/cajanegra/ReverseAndRemoveTests.java
@@ -1,117 +1,106 @@
 package com.practica.cajanegra;
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-import org.junit.runner.RunWith;
 
 import com.cajanegra.EmptyCollectionException;
 import com.cajanegra.SingleLinkedListImpl;
+import org.junit.jupiter.api.*;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertEquals;
 
 @DisplayName("Tests que incluyen los del tipo Reverse y Remove")
 class ReverseAndRemoveTests {
 
-	private static SingleLinkedListImpl<String> listaTest,listaVacia,listaUnElemento;
-	
-	@BeforeEach
-	public void setUp() {
-		this.listaTest = new SingleLinkedListImpl<String>("A","B","C","B");
-		this.listaVacia = new SingleLinkedListImpl<String>();
-		this.listaUnElemento = new SingleLinkedListImpl<String>("A");
-	}
-	
-	@Nested
-	@DisplayName("reverse()")
-	class reverse{
-		@Test
-		public void reverseCasoLleno() {
-			assertEquals("[B, C, B, A]", listaTest.reverse().toString());
-		}
-		@Test
-		public void reverseCasoVacio() {
-			assertEquals("[]", listaVacia.reverse().toString());
-		}
-		@Test
-		public void reverseCasoUnElemento() {
-			assertEquals("[A]", listaUnElemento.reverse().toString());
-		}
-	}
-	
-	@Nested
-	@DisplayName("removeLast()")
-	class removeLast{
-		@Test
-		public void removeLastCasoLLeno() throws EmptyCollectionException {
-			listaTest.removeLast();
-			assertEquals("[A, B, C]", listaTest.toString());	
-		}
-		@Test
-		public void removeLastCasoVacio() throws EmptyCollectionException {
-			Assertions.assertThrows(EmptyCollectionException.class, () -> {
-				listaVacia.removeLast();
-			});
-		}
-		@Test
-		public void removeLastCasoUnElemento() throws EmptyCollectionException {
-			listaUnElemento.removeLast();
-			assertEquals("[]", listaUnElemento.toString());
-		}
-	}
-	
-	@Nested
-	@DisplayName("removeLast(tElem) de [A, B, C, B]")
-	class removeLasttElem{
-		@Test
-		public void removeLastA() throws NoSuchElementException, EmptyCollectionException{
-			listaTest.removeLast("A");
-			assertEquals("[B, C, B]", listaTest.toString());
-		}
-		@Test
-		public void removeLastB() throws NoSuchElementException, EmptyCollectionException{
-			listaTest.removeLast("B");
-			assertEquals("[A, B, C]", listaTest.toString());
-		}
-		@Test
-		public void removeLastC() throws NoSuchElementException, EmptyCollectionException{
-			listaTest.removeLast("C");
-			assertEquals("[A, B, B]", listaTest.toString());
-		}
-		@Test
-		public void removeLastD() throws NoSuchElementException, EmptyCollectionException{
-			Assertions.assertThrows(NoSuchElementException.class, () -> {
-				listaTest.removeLast("D");
-			});
-		}
-		@Test
-		public void removeLastAEnListaVacía() throws NoSuchElementException, EmptyCollectionException{
-			Assertions.assertThrows(EmptyCollectionException.class, () -> {
-				listaVacia.removeLast("A");
-			});
-		}
-	}
+    private static SingleLinkedListImpl<String> listaTest, listaVacia, listaUnElemento;
+
+    @BeforeEach
+    public void setUp() {
+        this.listaTest = new SingleLinkedListImpl<String>("A", "B", "C", "B");
+        this.listaVacia = new SingleLinkedListImpl<String>();
+        this.listaUnElemento = new SingleLinkedListImpl<String>("A");
+    }
+
+    @Nested
+    @DisplayName("reverse()")
+    class reverse {
+        @Test
+        public void reverseCasoLleno() {
+            assertEquals("[B, C, B, A]", listaTest.reverse().toString());
+        }
+
+        @Test
+        public void reverseCasoVacio() {
+            assertEquals("[]", listaVacia.reverse().toString());
+        }
+
+        @Test
+        public void reverseCasoUnElemento() {
+            assertEquals("[A]", listaUnElemento.reverse().toString());
+        }
+    }
+
+    @Nested
+    @DisplayName("removeLast()")
+    class removeLast {
+        @Test
+        public void removeLastCasoLLeno() throws EmptyCollectionException {
+            listaTest.removeLast();
+            assertEquals("[A, B, C]", listaTest.toString());
+        }
+
+        @Test
+        public void removeLastCasoVacio() throws EmptyCollectionException {
+            Assertions.assertThrows(EmptyCollectionException.class, () -> {
+                listaVacia.removeLast();
+            });
+        }
+
+        @Test
+        public void removeLastCasoUnElemento() throws EmptyCollectionException {
+            listaUnElemento.removeLast();
+            assertEquals("[]", listaUnElemento.toString());
+        }
+    }
+
+    @Nested
+    @DisplayName("removeLast(tElem) de [A, B, C, B]")
+    class removeLasttElem {
+        @Test
+        public void removeLastA() throws NoSuchElementException, EmptyCollectionException {
+            listaTest.removeLast("A");
+            assertEquals("[B, C, B]", listaTest.toString());
+        }
+
+        @Test
+        public void removeLastB() throws NoSuchElementException, EmptyCollectionException {
+            listaTest.removeLast("B");
+            assertEquals("[A, B, C]", listaTest.toString());
+        }
+
+        @Test
+        public void removeLastC() throws NoSuchElementException, EmptyCollectionException {
+            listaTest.removeLast("C");
+            assertEquals("[A, B, B]", listaTest.toString());
+        }
+
+        @Test
+        public void removeLastD() throws NoSuchElementException, EmptyCollectionException {
+            Assertions.assertThrows(NoSuchElementException.class, () -> {
+                listaTest.removeLast("D");
+            });
+        }
+
+        @Test
+        public void removeLastAEnListaVacia() throws NoSuchElementException, EmptyCollectionException {
+            Assertions.assertThrows(EmptyCollectionException.class, () -> {
+                listaVacia.removeLast("A");
+            });
+        }
+    }
 	
 	
 	/*
-	// DUDA EN ESTE MÉTODO
+	// DUDA EN ESTE Mï¿½TODO
 	@ParameterizedTest(name="removeLast {0} in list")
     @CsvFileSource(resources = "/RemoveLastTelem.csv", numLinesToSkip = 0)
 	public void removeLast(String s) throws NoSuchElementException, EmptyCollectionException  {
@@ -135,7 +124,7 @@ class ReverseAndRemoveTests {
 						builder = builder.deleteCharAt(i);
 					} // CUANDO hay mas de un elemento repetido, en vez de eliminar el
 						// ultimo elimina todas las apariciones del elemento (no es lo que
-						// debe hacer el método)
+						// debe hacer el mï¿½todo)
 					aux = true;
 				}
 			}
@@ -155,7 +144,6 @@ class ReverseAndRemoveTests {
 		}
 	}
 	*/
-	
 
 
 }
